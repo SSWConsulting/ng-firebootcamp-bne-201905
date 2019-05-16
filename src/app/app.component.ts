72bbb80a27ab4e03452e4a3c69cb820f083cfb2b
brendan richards <brendan_richards_msdn@hotmail.com>
2019-05-16 11:51:26 +1000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firebootcamp-crm';
+
+  titleChanged(event) {
+    console.log('TITLE CHANGED', event);
+    this.title = event.target.value;
+  }
+
 }
